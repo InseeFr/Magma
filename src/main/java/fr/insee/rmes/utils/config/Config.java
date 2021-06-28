acb9f8ee902568cc9df7b99521fc4136e9d1cd46
Olivier Pucher <olivier.pucher@insee.fr>
2021-06-28 15:16:08 +0200

    improve Config

--- src/main/java/fr/insee/rmes/utils/config/Config.java
+++ src/main/java/fr/insee/rmes/utils/config/Config.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties
 public class Config {
 
-	public static final Object CODELIST_GRAPH = null; //TODO
-
 	public static final String BASE_URI_GESTION = null; //TODO
 	
 	@Value("${fr.insee.rmes.magma.lg1}")
@@ -18,7 +16,16 @@ public class Config {
 	@Value("${fr.insee.rmes.magma.concepts.baseURI}")
 	public static String CONCEPTS_BASE_URI;
 
+	@Value("${fr.insee.rmes.magma.structures.baseURI}")
+	public static String STRUCTURES_BASE_URI;
 
+	@Value("${fr.insee.rmes.magma.codeLists.baseURI}")
+	public static String CODELISTS_BASE_URI;
+	
+	@Value("${fr.insee.rmes.magma.codeLists.graph}")
+	public static Object CODELIST_GRAPH; 
+	
+	
 	private Config() {
 		throw new IllegalStateException("Utility class");
 	}
