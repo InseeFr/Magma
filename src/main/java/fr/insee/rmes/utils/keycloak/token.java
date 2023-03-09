7f783f20c2e463e898c45a7c6ca119a0ee491bcd
Jérôme Bourgeois <jerome.bourgeois@insee.fr>
2023-03-09 14:36:06 +0100

    Add properties + clean unused class
    Squashed commit of the following:
    
    commit 89269ebd11b6d00a6c89204ca6027639a099e8d8
    Author: Jérôme Bourgeois <jerome.bourgeois@insee.fr>
    Date:   Mon Dec 5 11:03:50 2022 +0100
    
        update Openid Connect
    
    commit ae284b407de1620d3b2d54001fb916163ec7dd96
    Merge: 0279111 9d75b0c
    Author: Jérôme Bourgeois <89983767+JeromeMBourgeois@users.noreply.github.com>
    Date:   Fri Dec 2 15:58:06 2022 +0100
    
        Merge pull request #35 from InseeFr/dv-statutValidation
    
        Dv statut validation
    
    commit 9d75b0c6057a3637e152191d95c97a73e4630a22
    Author: Hugo Bouttes <hugo.bouttes@insee.fr>
    Date:   Tue Nov 29 14:23:07 2022 +0100
    
        changement d'authentification
    
    commit 8c0fd020e181c233b32433bd4cc7e2019951b9e4
    Author: Hugo Bouttes <hugo.bouttes@insee.fr>
    Date:   Fri Nov 25 14:56:52 2022 +0100
    
        Changements ?
    
    commit 99da8132d50bc212feee602abd81b852f765e822
    Author: Hugo Bouttes <hugo.bouttes@insee.fr>
    Date:   Fri Nov 25 14:56:26 2022 +0100
    
        Changements ?
    
    commit 3d6716f85eb943d89f422e28ce9baee2f34c41ac
    Author: Hugo Bouttes <103732523+HugoBouttes@users.noreply.github.com>
    Date:   Mon Nov 7 16:58:47 2022 +0100
    
        Update rmeswsgi.properties
    
    commit 0916a9d54310433751494eeafb8e043f74ead6e0
    Author: Hugo Bouttes <hugo.bouttes@insee.fr>
    Date:   Mon Nov 7 16:36:25 2022 +0100
    
        modififcation de la requete sparql de concepts/{id} pour avoir un statutValidation cohérent

---
+++ src/main/java/fr/insee/rmes/utils/keycloak/token.java
@@ -0,0 +1,19 @@
+package fr.insee.rmes.utils.keycloak;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class token {
+    @JsonProperty("access_token")
+    String access_token;
+
+    private String token_type;
+
+    public String getAccessToken() {
+        return access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+}
