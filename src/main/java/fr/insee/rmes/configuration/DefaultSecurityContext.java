65abfd060dcfade9a3cc425b85848fb55c5e9f17
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2023-03-24 16:40:34 +0100

    fix(instantiate security context) bad @Conditional annotation to instantiate DefaultSecurityContext depending on fr.insee.rmes.magma.envir valeur #55

--- src/main/java/fr/insee/rmes/configuration/DefaultSecurityContext.java
+++ src/main/java/fr/insee/rmes/configuration/DefaultSecurityContext.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,7 +25,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled=false, prePostEnabled = true)
-@ConditionalOnProperty(name = "fr.insee.rmes.magma.envir", havingValue = "dev", matchIfMissing = true)
+@ConditionalOnExpression("!'PROD'.equals('${fr.insee.rmes.magma.envir}')")
 public class DefaultSecurityContext extends WebSecurityConfigurerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(DefaultSecurityContext.class);
