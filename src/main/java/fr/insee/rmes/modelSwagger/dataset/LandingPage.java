c6f67e1a24489f9f029b87cb0708e0f676a01d79
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2023-10-23 16:08:35 +0200

    fix : add @Getter and @Setter and remove null constructor from LandingPage

--- src/main/java/fr/insee/rmes/modelSwagger/dataset/LandingPage.java
+++ src/main/java/fr/insee/rmes/modelSwagger/dataset/LandingPage.java
@@ -3,6 +3,8 @@ package fr.insee.rmes.modelSwagger.dataset;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.annotation.Generated;
@@ -14,6 +16,8 @@ import javax.annotation.Generated;
         "url"
 })
 @Generated("jsonschema2pojo")
+@Getter
+@Setter
 public class LandingPage {
     @JsonProperty("lang")
     private String lang;
@@ -21,28 +25,9 @@ public class LandingPage {
     @JsonProperty("url")
     private String url;
 
-    public LandingPage() {
-    }
 
     public LandingPage(String lang, String url) {
         this.lang = lang;
-        this.url = url;
-    }
-
-    @JsonProperty("lang")
-    public String getLang() {
-        return lang;
-    }
-    @JsonProperty("lang")
-    public void setLang(String lang) {
-        this.lang = lang;
-    }
-    @JsonProperty("url")
-    public String getUrl() {
-        return url;
-    }
-    @JsonProperty("url")
-    public void setUrl(String url) {
         this.url = url;
     }
 }
