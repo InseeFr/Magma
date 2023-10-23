cdec73707cd9d5d19fb74d7e28ac7bf91c6c715e
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2023-10-23 16:08:15 +0200

    fix : add @Getter and @Setter and remove null constructor from Label

--- src/main/java/fr/insee/rmes/modelSwagger/dataset/Label.java
+++ src/main/java/fr/insee/rmes/modelSwagger/dataset/Label.java
@@ -3,6 +3,8 @@ package fr.insee.rmes.modelSwagger.dataset;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.annotation.Generated;
@@ -14,25 +16,15 @@ import java.util.List;
         "label"
 })
 @Generated("jsonschema2pojo")
+@Getter
+@Setter
 public class Label {
 
     @JsonProperty("label")
     private List<LangContent> label;
 
-    public Label() {
-    }
-
     public Label(List<LangContent> label) {
         this.label = label;
     }
 
-    @JsonProperty("label")
-    public List<LangContent> getLabel() {
-        return label;
-    }
-
-    @JsonProperty("label")
-    public void setLabel(List<LangContent> label) {
-        this.label = label;
-    }
 }
