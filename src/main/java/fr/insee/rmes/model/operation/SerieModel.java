7e2fea197d3345193c027e4fe57524825c233ce0
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2023-11-21 17:55:48 +0100

    fix: send empty array and non-null array when dc:creator does not exist

--- src/main/java/fr/insee/rmes/model/operation/SerieModel.java
+++ src/main/java/fr/insee/rmes/model/operation/SerieModel.java
@@ -1,17 +1,12 @@
 
 package fr.insee.rmes.model.operation;
 
+import com.fasterxml.jackson.annotation.*;
+import javax.annotation.Generated;
+import javax.validation.Valid;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-import javax.annotation.Generated;
-import javax.validation.Valid;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
