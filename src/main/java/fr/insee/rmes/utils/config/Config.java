11bfd12d62beabb3ea7416b47c48ef7aa4153df3
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2023-11-29 10:28:53 +0100

    fix: modify property

--- src/main/java/fr/insee/rmes/utils/config/Config.java
+++ src/main/java/fr/insee/rmes/utils/config/Config.java
@@ -1,8 +1,5 @@
 package fr.insee.rmes.utils.config;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 
 @Component
 
