5b378e1e5750ccfcb9726d0ce19bc6c8c8ee44ae
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2024-04-03 10:43:34 +0200

    refactor: datasets tests
    - remove the test getListDataSetsTest (no assertion) and replace it with a new one
    - remove the test getDataSetByIDTest  (no assertion). The method testPresenceVariablePuisAjoutTest_checkFieldIsAdded is a way to test the end-point Get dataset/{id}

--- src/test/java/fr/insee/rmes/services/datasets/DataSetsImplTest.java
+++ src/test/java/fr/insee/rmes/services/datasets/DataSetsImplTest.java
@@ -1,15 +1,17 @@
 package fr.insee.rmes.services.datasets;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.insee.rmes.model.datasets.*;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import fr.insee.rmes.model.datasets.Theme;
 import fr.insee.rmes.modelSwagger.dataset.*;
 import fr.insee.rmes.persistence.RepositoryGestion;
+import fr.insee.rmes.services.utils.DataSetsUtilsTest;
 import fr.insee.rmes.services.utils.ResponseUtilsTest;
 import fr.insee.rmes.stubs.FreeMarkerUtilsStub;
 import fr.insee.rmes.utils.config.Config;
 import fr.insee.rmes.utils.exceptions.RmesException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -24,131 +26,30 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.*;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class DataSetsImplTest {
+class DataSetsImplTest {
 
-    public static final String URI_TEST = "http://uri.test";
-    public static final String EMPTY_DATASET = "{}";
-    private static DataSetModelSwagger response=new DataSetModelSwagger();
+    private static final DataSetModelSwagger response=new DataSetModelSwagger();
     @InjectMocks
     DataSetsImpl dataSetsImpl=new DataSetsImpl(new FreeMarkerUtilsStub());
     @Mock
     RepositoryGestion repoGestion;
+    public static final ObjectMapper MAPPER = new JsonMapper();
 
     @Test
-    void getListDataSetsTest() throws JsonProcessingException {
-
-        DataSet dataSet1 = new DataSet("IdDataset1", "uriDataset1", "titreFrDataset1", "titreEnDataset1", "01/01/2022", "publié", "01/01/2021");
-        DataSet dataSet2 = new DataSet("IdDataset2", "uriDataset2", "titreFrDataset2", "titreEnDataset2", "01/01/2022", "provisoire", "01/01/2021");
-        List<DataSet> dataSets = new ArrayList<>();
-        dataSets.add(dataSet1);
-        dataSets.add(dataSet2);
-        ObjectMapper mapper = new ObjectMapper();
-        List<DataSetModelSwagger> dataSetListDTOS = new ArrayList<>();
-
-        for (DataSet byDataSet : dataSets) {
-            LangContent titre1 = new LangContent(Config.LG1, byDataSet.getTitreLg1());
-            LangContent titre2 = new LangContent(Config.LG2, byDataSet.getTitreLg2());
-            Id id1 = new Id(byDataSet.getId());
-            Uri uri = new Uri(byDataSet.getUri());
-            Modified modified = new Modified (byDataSet.getDateMiseAJour());
-            List<LangContent> titres = new ArrayList<>();
-            titres.add(titre1);
-            titres.add(titre2);
-            DataSetModelSwagger dataSetSwagger = new DataSetModelSwagger(id1, titres, uri, modified, byDataSet.getStatutValidation());
-            dataSetListDTOS.add(dataSetSwagger);
-        }
-
-        mapper.writeValueAsString(dataSetListDTOS);
-
-
+    void getListDataSetsTest() throws RmesException, JsonProcessingException {
+        JSONArray mockJSON = new JSONArray(DataSetsUtilsTest.DATA_SET_LIST);
+        when(repoGestion.getResponseAsArray(Mockito.anyString())).thenReturn(mockJSON);
+        assertThat(MAPPER.readTree(dataSetsImpl.getListDataSets())).isEqualTo(MAPPER.readTree(DataSetsUtilsTest.EXPECTED_GET_DATA_SET_LIST.toString()));
     }
 
-    @Test
-    void getDataSetByIDTest() {
-        DataSet dataSet = new DataSet("publie", "01/01/2021", "themeUn,ThemeDeux", "01/01/2022", "antecedent1/serie/1,antecedent1/serie/2,antecedent2/operation/1,antecedent2/operation/2", "nomFrDataset", "iddatset", "nomDataset", "UriDataset");
-        LangContent titre1 = new LangContent("fr", dataSet.getTitreLg1());
-        LangContent titre2 = new LangContent("en", dataSet.getTitreLg2());
-        List<LangContent> titres = new ArrayList<>();
-        titres.add(titre1);
-        titres.add(titre2);
-
-        String[] parts = dataSet.getNames().split(",");
-        List<ThemeModelSwagger> themeListDTOS = new ArrayList<>();
-
-        if (dataSet.getNames().endsWith("[]")) {
-            themeListDTOS = themeListDTOS;
-        } else {
-            for (int i = 0; i < Arrays.stream(parts).count(); i++) {
-                Theme theme1 = new Theme("uriTheme", "labelThemeFR", "labelThemeEn","themeTaxonomy");
-                LabelDataSet labelDataSet1 = new LabelDataSet(Config.LG1, theme1.getLabelThemeLg1());
-                LabelDataSet labelDataSet2 = new LabelDataSet(Config.LG2, theme1.getLabelThemeLg2());
-                List<LabelDataSet> labelDataSets = new ArrayList<>();
-                labelDataSets.add(labelDataSet1);
-                labelDataSets.add(labelDataSet2);
-                ThemeModelSwagger themeSwagger = new ThemeModelSwagger(theme1.getUri(), labelDataSets, theme1.getThemeTaxonomy());
-                themeListDTOS.add(themeSwagger);
-
-            }
-        }
-        List<String> operationStat = List.of(dataSet.getOperationStat().split(","));
-
-        Stream<String> streamSerie = operationStat.stream();
-        List<String> serieUri = streamSerie.filter(v -> v.contains("/serie/"))
-                .collect(Collectors.toList());
-
-
-        List<SerieModelSwagger> serieListSwaggerS = new ArrayList<>();
-        for (String value : serieUri) {
-            Serie serie1 = new Serie("uriSerie", "idSerie", "labelSerieFr", "labelSerieEn");
-            LabelDataSet labelDataSet1 = new LabelDataSet(Config.LG1, serie1.getLabelSerieLg1());
-            LabelDataSet labelDataSet2 = new LabelDataSet(Config.LG2, serie1.getLabelSerieLg2());
-            List<LabelDataSet> labelDataSets = new ArrayList<>();
-            labelDataSets.add(labelDataSet1);
-            labelDataSets.add(labelDataSet2);
-            SerieModelSwagger serieSwagger = new SerieModelSwagger(serie1.getUri(), serie1.getId(), labelDataSets);
-            serieListSwaggerS.add(serieSwagger);
-        }
-        List<SerieModelSwagger> serieListDTOS = serieListSwaggerS;
-
-        Stream<String> streamOperation = operationStat.stream();
-        List<String> operationUri = streamOperation.filter(v -> v.contains("/operation/"))
-                .collect(Collectors.toList());
-        List<OperationModelSwagger> operationListSwaggerS = new ArrayList<>();
-        for (String s : operationUri) {
-            Operation operation1 = new Operation("uriOp1", "idOp1", "labelOp1", "labelOp1");
-            LabelDataSet labelDataSet1 = new LabelDataSet(Config.LG1, operation1.getlabelOperationLg1());
-            LabelDataSet labelDataSet2 = new LabelDataSet(Config.LG2, operation1.getlabelOperationLg2());
-            List<LabelDataSet> labelDataSets = new ArrayList<>();
-            labelDataSets.add(labelDataSet1);
-            labelDataSets.add(labelDataSet2);
-            OperationModelSwagger operationSwagger = new OperationModelSwagger(operation1.getUri(), operation1.getId(), labelDataSets);
-            operationListSwaggerS.add(operationSwagger);
-        }
-        List<OperationModelSwagger> operationListDTOS = operationListSwaggerS;
-        Id id1 = new Id(dataSet.getId());
-        Uri uri = new Uri(dataSet.getUri());
-        Modified modified = new Modified (dataSet.getDateMiseAJour());
-        DataSetModelSwagger dataSetSwagger = new DataSetModelSwagger(id1, titres, uri, modified, dataSet.getStatutValidation(), themeListDTOS);
-        ObjectMapper dataSetFinal = new ObjectMapper();
-        JsonNode dataSetFinalNode = dataSetFinal.valueToTree(dataSetSwagger);
-        Iterator<JsonNode> it = dataSetFinalNode.iterator();
-
-        while (it.hasNext()) {
-            JsonNode node = it.next();
-            if (node.isContainerNode() && node.isEmpty()) {
-                it.remove();
-            }
-        }
-    }
 
     @Test
     void setTitreListTest(){
@@ -157,7 +58,7 @@ public class DataSetsImplTest {
         List<LangContent> titres = new ArrayList<>();
         titres.add(titre1);
         titres.add(titre2);
-        assertThat(titres.toString()).isEqualTo(ResponseUtilsTest.EXPECTED_JSON_SET_TITRE_LIST);
+        assertThat(titres).hasToString(DataSetsUtilsTest.EXPECTED_JSON_SET_TITRE_LIST);
     }
 
 
@@ -175,7 +76,7 @@ public class DataSetsImplTest {
         params.put("ORGANISATIONS_GRAPH","OrganisationsGraphTest");
         params.put("OPERATIONS_GRAPH","OperationsGraphTest");
         params.put("ONTOLOGIES_GRAPH","OntologiesGraphTest");
-        assertTrue(params.toString().equals(ResponseUtilsTest.EXPECTED_MAP_INIT_PARAMS));
+        assertEquals(DataSetsUtilsTest.EXPECTED_MAP_INIT_PARAMS,params.toString());
     }
 
 
@@ -188,9 +89,9 @@ public class DataSetsImplTest {
         langContentList.add(langContent1);
         langContentList.add(langContent2);
         IdLabel idlabeltest = new IdLabel("id",langContentList);
-        assertTrue(idlabeltest.toString().equals(ResponseUtilsTest.EXPECTED_LANGCONTENTLIST_SET_ID_LABEL));
+        assertEquals(DataSetsUtilsTest.EXPECTED_LANGCONTENTLIST_SET_ID_LABEL, idlabeltest.toString());
         idlabeltest.setType("type");
-        assertTrue(idlabeltest.toString().equals(ResponseUtilsTest.EXPECTED_LANGCONTENTLIST_SET_ID_LABEL_WITH_TYPE));
+        assertEquals(DataSetsUtilsTest.EXPECTED_LANGCONTENTLIST_SET_ID_LABEL_WITH_TYPE, idlabeltest.toString());
     }
 
     //TODO petite méthode d'extraction des URIs
@@ -203,7 +104,7 @@ public class DataSetsImplTest {
             Map<String, Object> params = new HashMap<>();
             params.put("URI",parts[i].replace(" ", ""));
             String expectedParams = "{URI=uri"+i+"}";
-            assertTrue(params.toString().equals(expectedParams));
+            assertEquals(expectedParams,params.toString());
             params.remove("URI");
             Theme themetest = new Theme("uri"+i,"labelThemeLg1"+i,"labelThemeLg2"+i,"themeTaxonomy"+i);
             LabelDataSet labelDataSet1 = new LabelDataSet("fr", themetest.getLabelThemeLg1());
@@ -214,7 +115,7 @@ public class DataSetsImplTest {
             ThemeModelSwagger themeModelSwagger = new ThemeModelSwagger(themetest.getUri(), labelDataSets,themetest.getThemeTaxonomy());
             themeListModelSwaggerS.add(themeModelSwagger);
         }
-        assertTrue(themeListModelSwaggerS.toString().equals(ResponseUtilsTest.EXPECTED_THEMELIST_GET_THEME_MODEL_SWAGGERS));
+        assertEquals(DataSetsUtilsTest.EXPECTED_THEMELIST_GET_THEME_MODEL_SWAGGERS, themeListModelSwaggerS.toString());
     }
 
     @ParameterizedTest(name = "{0}")
@@ -243,7 +144,7 @@ public class DataSetsImplTest {
     }
     @Test
     void getDataSetByIDDateMiseAJourFalse_shouldReturn404IfInexistentId() throws RmesException {
-        JSONObject mockJSON = new JSONObject(EMPTY_DATASET);
+        JSONObject mockJSON = new JSONObject(ResponseUtilsTest.EMPTY_JSON_OBJECT);
         when(repoGestion.getResponseAsObject(Mockito.anyString())).thenReturn(mockJSON);
 
         assertThatThrownBy(()->dataSetsImpl.findDataSetModelSwagger("1")).isInstanceOf(RmesException.class)
@@ -253,12 +154,14 @@ public class DataSetsImplTest {
 
     @Test
     void getDataSetByIDDateMiseAJour_shouldReturn404IfInexistentId() throws RmesException {
-        JSONObject mockJSON = new JSONObject(EMPTY_DATASET);
+        JSONObject mockJSON = new JSONObject(ResponseUtilsTest.EMPTY_JSON_OBJECT);
         when(repoGestion.getResponseAsObject(Mockito.anyString())).thenReturn(mockJSON);
 
         assertThatThrownBy(()->dataSetsImpl.getDataSetByIDSummary("1")).isInstanceOf(RmesException.class)
                 .matches(rmesException->((RmesException)rmesException).getStatus()==404)
                 .hasMessageContaining("Non existent dataset identifier");
     }
+
+
 
 }
