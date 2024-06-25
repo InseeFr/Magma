e0ba0d64fa2389da07f53ac0600e4eed50739607
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2024-06-25 11:35:23 +0200

    fix: survey parameter
    in low-case instead of upper-case (as in prod)

--- src/main/java/fr/insee/rmes/controller/PoguesResources.java
+++ src/main/java/fr/insee/rmes/controller/PoguesResources.java
@@ -13,6 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,19 +24,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
-import jakarta.ws.rs.core.MediaType;
 import java.io.IOException;
 
 
 @RestController
-@RequestMapping(value="/",produces = {"application/json"})
+@RequestMapping(value = "/", produces = {"application/json"})
 @Tag(name = "Series-Opérations", description = "Consultation des Séries et Opérations de la base de gestion")
 @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Success",content = {@Content }),
-        @ApiResponse(responseCode = "404", description = "Not found",content = {@Content }),
-        @ApiResponse(responseCode = "500", description = "Internal server error",content = {@Content })})
+        @ApiResponse(responseCode = "200", description = "Success", content = {@Content}),
+        @ApiResponse(responseCode = "404", description = "Not found", content = {@Content}),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = {@Content})})
 
 public class PoguesResources {
 
@@ -43,11 +43,11 @@ public class PoguesResources {
 
     @GetMapping("/operations/series")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "getAllSeries", summary = "Get all series",security = @SecurityRequirement(name = "bearerScheme"),
+    @Operation(operationId = "getAllSeries", summary = "Get all series", security = @SecurityRequirement(name = "bearerScheme"),
             responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = SerieByIdModelSwagger.class)))})
     public ResponseEntity<String> getAllSeriesLists(@Parameter(
             description = "param for survey only",
-            required = false) @QueryParam("Survey") Boolean survey) throws RmesException, IOException {
+            required = false) @QueryParam("survey") Boolean survey) throws RmesException, IOException {
 
         String jsonResult = (String) poguesServices.getAllSeriesLists(survey);
 
@@ -61,7 +61,7 @@ public class PoguesResources {
 
     @GetMapping("/operations/serie/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "getSerieById", summary = "Get one serie",security = @SecurityRequirement(name = "bearerScheme"),
+    @Operation(operationId = "getSerieById", summary = "Get one serie", security = @SecurityRequirement(name = "bearerScheme"),
             responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = SerieByIdModelSwagger.class)))})
 
     public ResponseEntity<String> getCodeList(@PathVariable("id") String id) throws RmesException, IOException {
@@ -76,7 +76,7 @@ public class PoguesResources {
 
     @GetMapping("/operations/serie/{id}/operations")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "getOperationsBySerie", summary = "Get operations by serie",security = @SecurityRequirement(name = "bearerScheme"),
+    @Operation(operationId = "getOperationsBySerie", summary = "Get operations by serie", security = @SecurityRequirement(name = "bearerScheme"),
             responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = OperationBySerieIdModelSwagger.class)))})
 
     public ResponseEntity<String> getOperationsBySerie(@PathVariable("id") String id) throws RmesException, IOException {
@@ -90,7 +90,7 @@ public class PoguesResources {
 
     @GetMapping("/operations/operation/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Operation(operationId = "getOperationsBycode", summary = "Get operations by code",security = @SecurityRequirement(name = "bearerScheme"),
+    @Operation(operationId = "getOperationsBycode", summary = "Get operations by code", security = @SecurityRequirement(name = "bearerScheme"),
             responses = {@ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = OperationByIdModelSwagger.class)))})
 
     public ResponseEntity<String> getOperationByCode(@PathVariable("id") String id) throws RmesException, IOException {
