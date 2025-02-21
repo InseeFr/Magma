c60ad7ec8fc6d4e7da43740115a4bbf5952ba15b
SMaSndil <sofiane.maazi@insee.fr>
2025-02-21 10:05:04 +0100

    delete : remove comments and fields idWithTypeId/uriWithTypeUri from get/dataset/list json output.

--- src/main/java/fr/insee/rmes/modelSwagger/dataset/DataSetModelSwagger.java
+++ src/main/java/fr/insee/rmes/modelSwagger/dataset/DataSetModelSwagger.java
@@ -195,18 +195,4 @@ public class DataSetModelSwagger implements Serializable  {
         this.validationState = validationState;
         this.themeModelSwaggerS = themeModelSwaggerS;
     }
-
-    // If we want to add an "idWithTypeId" field, we have to add line below this comment.
-    // Then it has to modify DATA_SET_LIST in DataSetsUtilsTest.java.
-    // public Id getIdWithTypeId(){
-    //   Id id = new Id(this.id);
-    //   return id;
-    // }
-
-    // If we want to add an "uriWithTypeId" field, we have to add line below this comment.
-    // Then it has to modify EXPECTED_GET_DATA_SET_LIST in DataSetsUtilsTest.java.
-    // public Uri getUriWithTypeUri(){
-    //   Uri uri = new Uri(this.uri);
-    //   return uri;
-    // }
 }
