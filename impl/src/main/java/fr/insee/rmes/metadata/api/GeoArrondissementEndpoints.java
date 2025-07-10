388a77ffd243a633838abb493f7e5a151d53ad52
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-07-10 16:53:27 +0200

    feat: for geo/arrondissement/{code}/precedents and geo/arrondissementMunicipal/{code}/precedents

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoArrondissementEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoArrondissementEndpoints.java
@@ -7,6 +7,7 @@ import fr.insee.rmes.metadata.model.TerritoireTousAttributs;
 import fr.insee.rmes.metadata.model.TypeEnumAscendantsArrondissement;
 import fr.insee.rmes.metadata.model.TypeEnumDescendantsArrondissement;
 import fr.insee.rmes.metadata.queries.parameters.AscendantsDescendantsRequestParametizer;
+import fr.insee.rmes.metadata.queries.parameters.PrecedentsSuivantsRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,7 +25,7 @@ public class GeoArrondissementEndpoints implements GeoArrondissementApi {
     }
 
     @Override
-    public ResponseEntity<List<TerritoireTousAttributs>>  getcogarrasc (String code, LocalDate date, TypeEnumAscendantsArrondissement type) {
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogarrasc(String code, LocalDate date, TypeEnumAscendantsArrondissement type) {
         return requestProcessor.queryforFindAscendantsDescendants()
                 .with(new AscendantsDescendantsRequestParametizer(code, date, type, Arrondissement.class))
                 .executeQuery()
@@ -41,7 +42,7 @@ public class GeoArrondissementEndpoints implements GeoArrondissementApi {
     }
 
     @Override
-    public ResponseEntity<List<TerritoireTousAttributs>>  getcogarrdes (String code, LocalDate date, TypeEnumDescendantsArrondissement type) {
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogarrdes(String code, LocalDate date, TypeEnumDescendantsArrondissement type) {
         return requestProcessor.queryforFindAscendantsDescendants()
                 .with(new AscendantsDescendantsRequestParametizer(code, date, type, Arrondissement.class))
                 .executeQuery()
@@ -50,13 +51,22 @@ public class GeoArrondissementEndpoints implements GeoArrondissementApi {
     }
 
     @Override
-    public ResponseEntity<List<Arrondissement>> getcogarrliste (LocalDate date) {
+    public ResponseEntity<List<Arrondissement>> getcogarrliste(LocalDate date) {
         return requestProcessor.queryforFindTerritoire()
                 .with(new TerritoireRequestParametizer(date, Arrondissement.class, "*"))
                 .executeQuery()
                 .listResult(Arrondissement.class)
                 .toResponseEntity();
 
+    }
+
+    @Override
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogarrprec(String code, LocalDate date) {
+        return requestProcessor.queryforFindPrecedentsSuivants()
+                .with(new PrecedentsSuivantsRequestParametizer(code, date, Arrondissement.class, true))
+                .executeQuery()
+                .listResult(TerritoireTousAttributs.class)
+                .toResponseEntity();
     }
 
 }
--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoArrondissementMunipalEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoArrondissementMunipalEndpoints.java
@@ -1,8 +1,11 @@
 package fr.insee.rmes.metadata.api;
 
 import fr.insee.rmes.metadata.api.requestprocessor.RequestProcessor;
-import fr.insee.rmes.metadata.model.*;
+import fr.insee.rmes.metadata.model.ArrondissementMunicipal;
+import fr.insee.rmes.metadata.model.TerritoireTousAttributs;
+import fr.insee.rmes.metadata.model.TypeEnumAscendantsArrondissementMunicipal;
 import fr.insee.rmes.metadata.queries.parameters.AscendantsDescendantsRequestParametizer;
+import fr.insee.rmes.metadata.queries.parameters.PrecedentsSuivantsRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,7 +24,7 @@ public class GeoArrondissementMunipalEndpoints implements GeoArrondissementMunic
     }
 
     @Override
-    public ResponseEntity<List<TerritoireTousAttributs>>  getcogarrmuasc (String code, LocalDate date, TypeEnumAscendantsArrondissementMunicipal type) {
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogarrmuasc(String code, LocalDate date, TypeEnumAscendantsArrondissementMunicipal type) {
         return requestProcessor.queryforFindAscendantsDescendants()
                 .with(new AscendantsDescendantsRequestParametizer(code, date, type, ArrondissementMunicipal.class))
                 .executeQuery()
@@ -30,7 +33,7 @@ public class GeoArrondissementMunipalEndpoints implements GeoArrondissementMunic
     }
 
     @Override
-    public ResponseEntity<ArrondissementMunicipal>  getcogarrmu (String code, LocalDate date) {
+    public ResponseEntity<ArrondissementMunicipal> getcogarrmu(String code, LocalDate date) {
         return requestProcessor.queryforFindTerritoire()
                 .with(new TerritoireRequestParametizer(code, date, ArrondissementMunicipal.class, "none"))
                 .executeQuery()
@@ -38,13 +41,22 @@ public class GeoArrondissementMunipalEndpoints implements GeoArrondissementMunic
     }
 
     @Override
-    public ResponseEntity<List<ArrondissementMunicipal>> getcogarrmuliste (LocalDate date) {
+    public ResponseEntity<List<ArrondissementMunicipal>> getcogarrmuliste(LocalDate date) {
         return requestProcessor.queryforFindTerritoire()
                 .with(new TerritoireRequestParametizer(date, ArrondissementMunicipal.class, "none"))
                 .executeQuery()
                 .listResult(ArrondissementMunicipal.class)
                 .toResponseEntity();
 
+    }
+
+    @Override
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogarrmuprec(String code, LocalDate date) {
+        return requestProcessor.queryforFindPrecedentsSuivants()
+                .with(new PrecedentsSuivantsRequestParametizer(code, date, ArrondissementMunicipal.class, true))
+                .executeQuery()
+                .listResult(TerritoireTousAttributs.class)
+                .toResponseEntity();
     }
 
 }
