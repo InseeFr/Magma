0f5e70fbd03479df1bd47a4c9b45a13d086b1c3e
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-07-16 11:05:28 +0200

    fix: add constants

--- impl/src/main/java/fr/insee/rmes/metadata/queries/QueryBuilder.java
+++ impl/src/main/java/fr/insee/rmes/metadata/queries/QueryBuilder.java
@@ -14,16 +14,18 @@ import java.util.Map;
 
 @Component
 @Slf4j
-public record QueryBuilder (Configuration freemarkerConfiguration) {
+public record QueryBuilder(Configuration freemarkerConfiguration) {
     public static final Path ASCENDANTS_OR_DESCENDANTS = Path.of("geographie/getAscendantsOrDescendantsByCodeTypeDate.ftlh");
     public static final Path TERRITOIRE = Path.of("geographie/getTerritoireByCodeDateNomCommune.ftlh");
     public static final Path PRECEDENTS = Path.of("geographie/getPreviousOrNextByCodeTypeDate.ftlh");
     public static final Path PROJETES = Path.of("geographie/getProjectionByCodeTypeDate.ftlh");
-//    public static final Path IRIS = Path.of("geographie/getIrisByCodeDate.ftlh");
+    //    public static final Path IRIS = Path.of("geographie/getIrisByCodeDate.ftlh");
     public static final Path IRIS = Path.of("geographie/getTerritoireByCodeDateNomCommune.ftlh");
     public static final Path LIEN_COMMUNE_IRIS = Path.of("geographie/hasIrisDescendant.ftlh");
     public static final Path LIEN_PAYS = Path.of("geographie/getPays.ftlh");
     public static final Path DESCENDANTS_PAYS = Path.of("geographie/getPaysDescendants.ftlh");
+    public static final Path PAYS_PRECEDENTS = Path.of("geographie/getPaysPrecedents.ftlh");
+    public static final Path PAYS_SUIVANTS = Path.of("geographie/getPaysSuivants.ftlh");
 
 
     public Query build(Map<String, Object> parameters, Path queryfile) {
@@ -32,8 +34,7 @@ public record QueryBuilder (Configuration freemarkerConfiguration) {
         try {
             template = freemarkerConfiguration.getTemplate(queryfile.toString());
             template.process(parameters, out);
-        }
-        catch (IOException | TemplateException e) {
+        } catch (IOException | TemplateException e) {
             //TODO lever une Exception non contrôlée et utiliser un ExceptionHandler au niveau des contrôleurs pour traiter l'erreur
             log.error("Can't read query {} : ", queryfile, e);
         }
