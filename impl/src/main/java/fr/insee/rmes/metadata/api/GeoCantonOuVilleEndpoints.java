2f72f8cf4616d999d19d336815819c25ff08471b
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-07-17 15:56:28 +0200

    feat: for geo/caontouOuVilles/{code}/projetes

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoCantonOuVilleEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoCantonOuVilleEndpoints.java
@@ -7,6 +7,7 @@ import fr.insee.rmes.metadata.model.TypeEnumAscendantsCantonOuVille;
 import fr.insee.rmes.metadata.model.TypeEnumDescendantsCantonOuVille;
 import fr.insee.rmes.metadata.queries.parameters.AscendantsDescendantsRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.PrecedentsSuivantsRequestParametizer;
+import fr.insee.rmes.metadata.queries.parameters.ProjetesRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -67,6 +68,22 @@ public class GeoCantonOuVilleEndpoints implements GeoCantonEtVilleApi {
     public ResponseEntity<List<TerritoireTousAttributs>> getcogcanvilprec(String code, LocalDate date) {
         return requestProcessor.queryforFindPrecedentsSuivants()
                 .with(new PrecedentsSuivantsRequestParametizer(code, date, CantonOuVille.class, true))
+                .executeQuery()
+                .listResult(TerritoireTousAttributs.class)
+                .toResponseEntity();
+    }
+
+    @Override
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogcanvilproj(String code, LocalDate dateProjection, LocalDate date) {
+        //le booléen previous est calculé en fonction du paramètre dateProjection (paramètre obligatoire) et du paramètre date valorisé à la date du jour si absent
+        // (facultatif). La valorisation de date à la date du jour dans ParameterValueDecoder n'est pas conservée en dehors de la méthode
+        // => obligé de valoriser date ici aussi
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        boolean previous = !dateProjection.isAfter(date);
+        return requestProcessor.queryforFindProjetes()
+                .with(new ProjetesRequestParametizer(code, dateProjection, date, CantonOuVille.class, previous))
                 .executeQuery()
                 .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
