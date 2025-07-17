cf3dc8daa6fa62756b12561f151636d6018d0024
Guylene Tandeau <guylene.tandeau-de-marsac@insee.fr>
2025-07-17 16:35:51 +0200

    feat: for geo/intercommunalite/{code}/projetes

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoIntercommunaliteEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoIntercommunaliteEndpoints.java
@@ -6,6 +6,7 @@ import fr.insee.rmes.metadata.model.TerritoireTousAttributs;
 import fr.insee.rmes.metadata.model.TypeEnumDescendantsIntercommunalite;
 import fr.insee.rmes.metadata.queries.parameters.AscendantsDescendantsRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.PrecedentsSuivantsRequestParametizer;
+import fr.insee.rmes.metadata.queries.parameters.ProjetesRequestParametizer;
 import fr.insee.rmes.metadata.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -58,6 +59,23 @@ public class GeoIntercommunaliteEndpoints implements GeoIntercommunaliteApi {
     public ResponseEntity<List<TerritoireTousAttributs>> getcogintercoprec(String code, LocalDate date) {
         return requestProcessor.queryforFindPrecedentsSuivants()
                 .with(new PrecedentsSuivantsRequestParametizer(code, date, Intercommunalite.class, true))
+                .executeQuery()
+                .listResult(TerritoireTousAttributs.class)
+                .toResponseEntity();
+    }
+
+
+    @Override
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogintercoproj(String code, LocalDate dateProjection, LocalDate date) {
+        //le booléen previous est calculé en fonction du paramètre dateProjection (paramètre obligatoire) et du paramètre date valorisé à la date du jour si absent
+        // (facultatif). La valorisation de date à la date du jour dans ParameterValueDecoder n'est pas conservée en dehors de la méthode
+        // => obligé de valoriser date ici aussi
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        boolean previous = !dateProjection.isAfter(date);
+        return requestProcessor.queryforFindProjetes()
+                .with(new ProjetesRequestParametizer(code, dateProjection, date, Intercommunalite.class, previous))
                 .executeQuery()
                 .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
