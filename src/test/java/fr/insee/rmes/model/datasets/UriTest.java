36d16850091875c677a957cbc45dd794ca77f309
SM <sofiane.maazi@insee.fr>
2025-08-21 14:52:29 +0200

    feat: improve method

--- src/test/java/fr/insee/rmes/model/datasets/UriTest.java
+++ src/test/java/fr/insee/rmes/model/datasets/UriTest.java
@@ -1,14 +1,16 @@
 package fr.insee.rmes.model.datasets;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UriTest {
 
-    @Test
-    void shouldReturnStringWhenToString(){
-        Uri uri1  = new Uri("mockedUri");
-        assertEquals("mockedUri",uri1.toString());
+    @ParameterizedTest
+    @ValueSource(strings = { "element1", "element2","element3" })
+    void shouldReturnStringWhenToString(String string){
+        Uri uri  = new Uri(string);
+        assertEquals(string,uri.toString());
     }
- 
+
 }
