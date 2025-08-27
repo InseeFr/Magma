f6741ffbf7d41cf7b9185753dae99d87906def05
SM <sofiane.maazi@insee.fr>
2025-08-27 12:25:28 +0200

    feat: sonar issue

--- src/test/java/fr/insee/rmes/model/datasets/DisseminationStatusTest.java
+++ src/test/java/fr/insee/rmes/model/datasets/DisseminationStatusTest.java
@@ -11,5 +11,4 @@ class DisseminationStatusTest {
         DisseminationStatus disseminationStatus = new DisseminationStatus(string);
         assertEquals(string,disseminationStatus.toString());
     }
-  
 }
