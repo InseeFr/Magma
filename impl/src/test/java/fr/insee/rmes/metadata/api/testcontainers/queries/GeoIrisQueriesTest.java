cbc511ee84a83efe954de242df25c93d51452e7c
Tandeau De Marsac Guylène <C3JGPV@ad.insee.intra>
2025-10-07 14:33:18 +0200

    fix: for geo/iris/{code}/ascendants when type parameter is defined

--- impl/src/test/java/fr/insee/rmes/metadata/api/testcontainers/queries/GeoIrisQueriesTest.java
+++ impl/src/test/java/fr/insee/rmes/metadata/api/testcontainers/queries/GeoIrisQueriesTest.java
@@ -3,6 +3,7 @@ package fr.insee.rmes.metadata.api.testcontainers.queries;
 import fr.insee.rmes.metadata.api.GeoIntercommunaliteEndpoints;
 import fr.insee.rmes.metadata.api.GeoIrisEndpoints;
 import fr.insee.rmes.metadata.model.TerritoireTousAttributs;
+import fr.insee.rmes.metadata.model.TypeEnumAscendantsIris;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -52,7 +53,29 @@ public class GeoIrisQueriesTest  extends TestcontainerTest {
         );
     }
 
-//    geo/iris/010040101/ascendants?date=2025-09-04 (false-Iris)
+//    geo/iris/010040101/ascendants?date=2025-09-04 (real Iris)
+    @Test
+    void should_return_1_arrondissement_when_IrisCodeAscendants_Code010040101_date20250904_typeArrondissement(){
+        var response  = endpoints.getcogirisasc ("010040101", LocalDate.of(2025,9,4), TypeEnumAscendantsIris.ARRONDISSEMENT);
+        var result = response.getBody();
+        Assertions.assertNotNull(result);
+        var resultItem1= result.getFirst();
+
+        assertAll(
+                () -> assertEquals(1, result.size()),
+                () -> assertEquals("011", resultItem1.getCode()),
+                () -> assertEquals("http://id.insee.fr/geo/arrondissement/cc3aee67-96dc-4e9a-ae4e-26860a90e0d5", resultItem1.getUri()),
+                () -> assertEquals(TerritoireTousAttributs.TypeEnum.ARRONDISSEMENT, resultItem1.getType()),
+                () -> assertEquals(LocalDate.of(2017,1,1), resultItem1.getDateCreation()),
+                () -> assertEquals("Belley", resultItem1.getIntituleSansArticle()),
+                () -> assertEquals(TerritoireTousAttributs.TypeArticleEnum._0 , resultItem1.getTypeArticle()),
+                () -> assertEquals("01034" , resultItem1.getChefLieu()),
+                () -> assertEquals("Belley", resultItem1.getIntitule())
+
+        );
+    }
+
+//    geo/iris/010050000/ascendants?date=2025-09-04 (= false-Iris = non irised commune)
     @Test
     void should_return_10_territoires_when_IrisCodeAscendants_Code010050000_date20250904_typeNull(){
         var response  = endpoints.getcogirisasc ("010050000", LocalDate.of(2025,9,4), null);
@@ -73,6 +96,27 @@ public class GeoIrisQueriesTest  extends TestcontainerTest {
         );
     }
 
+//    geo/iris/010050000/ascendants?date=2025-09-04?type=arrondissement (false-Iris)
+    @Test
+    void should_return_1_arrondissement_when_IrisCodeAscendants_Code010050000_date20250904_typeArrondissement(){
+        var response  = endpoints.getcogirisasc ("010050000", LocalDate.of(2025,9,4), TypeEnumAscendantsIris.ARRONDISSEMENT);
+        var result = response.getBody();
+        Assertions.assertNotNull(result);
+        var resultItem1= result.getFirst();
+
+        assertAll(
+                () -> assertEquals(1, result.size()),
+                () -> assertEquals("012", resultItem1.getCode()),
+                () -> assertEquals("http://id.insee.fr/geo/arrondissement/34ccfd7d-aeeb-4c1d-ae46-c989a82d05b0", resultItem1.getUri()),
+                () -> assertEquals(TerritoireTousAttributs.TypeEnum.ARRONDISSEMENT, resultItem1.getType()),
+                () -> assertEquals(LocalDate.of(2017,1,1), resultItem1.getDateCreation()),
+                () -> assertEquals("Bourg-en-Bresse", resultItem1.getIntituleSansArticle()),
+                () -> assertEquals(TerritoireTousAttributs.TypeArticleEnum._0 , resultItem1.getTypeArticle()),
+                () -> assertEquals("Bourg-en-Bresse", resultItem1.getIntitule())
+
+        );
+    }
+
     ////////////////////////////////////////////////////////////////////
     ///                         geo/iris                             ///
     ////////////////////////////////////////////////////////////////////
@@ -82,6 +126,7 @@ public class GeoIrisQueriesTest  extends TestcontainerTest {
     void should_return_49343_territoires_when_Iris_date20250904(){
         var response  = endpoints.getcogirislist (LocalDate.of(2025,9,4), null);
         var result = response.getBody();
+        Assertions.assertNotNull(result);
         var resultItem1= result.getFirst();
 
         assertAll(
@@ -102,6 +147,7 @@ public class GeoIrisQueriesTest  extends TestcontainerTest {
     void should_return_49444_territoires_when_Iris_date20250904_comTrue(){
         var response  = endpoints.getcogirislist (LocalDate.of(2025,9,4), true);
         var result = response.getBody();
+        Assertions.assertNotNull(result);
         var resultItem1= result.getFirst();
 
         assertAll(
