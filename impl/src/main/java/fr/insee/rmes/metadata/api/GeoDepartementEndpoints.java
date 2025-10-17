c55360300fab31a57df9f11e3bdff2ca99204cbf
Tandeau De Marsac Guylène <C3JGPV@ad.insee.intra>
2025-10-17 11:02:07 +0200

    refactor: one type of territory instead of 3

--- impl/src/main/java/fr/insee/rmes/metadata/api/GeoDepartementEndpoints.java
+++ impl/src/main/java/fr/insee/rmes/metadata/api/GeoDepartementEndpoints.java
@@ -42,21 +42,21 @@ public class GeoDepartementEndpoints implements GeoDepartementApi {
     }
 
     @Override
-    public ResponseEntity<List<TerritoireBaseChefLieu>>  getcogdepprec(String code, LocalDate date) {
+    public ResponseEntity<List<TerritoireTousAttributs>>  getcogdepprec(String code, LocalDate date) {
         return requestProcessor.queryforFindPrecedentsSuivants()
                 .with(new PrecedentsSuivantsRequestParametizer(code, date, Departement.class, true))
                 .executeQuery()
-                .listResult(TerritoireBaseChefLieu.class)
+                .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
     }
 
 
 
     @Override
-    public ResponseEntity<List<TerritoireBaseChefLieu>>  getcogdepproj(String code, LocalDate dateProjection, LocalDate date) {
-        //le booléen previous est calculé en fonction du paramètre dateProjection (paramètre obligatoire) et du paramètre date valorisé à la date du jour si absent
-        // (facultatif). La valorisation de date à la date du jour dans ParameterValueDecoder n'est pas conservée en dehors de la méthode
-        // => obligé de valoriser date ici aussi
+    public ResponseEntity<List<TerritoireTousAttributs>>  getcogdepproj(String code, LocalDate dateProjection, LocalDate date) {
+        //The Boolean previous is based on the dateProjection parameter (required parameter) and on the date parameter set to today's date if absent
+        // (optional). Setting the date to today's date in ParameterValueDecoder is not retained outside the method
+        // => must set the date here as well
         if (date == null) {
             date = LocalDate.now();
         }
@@ -64,16 +64,16 @@ public class GeoDepartementEndpoints implements GeoDepartementApi {
         return requestProcessor.queryforFindProjetes()
                 .with(new ProjetesRequestParametizer(code, dateProjection, date, Departement.class, previous))
                 .executeQuery()
-                .listResult(TerritoireBaseChefLieu.class)
+                .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
     }
 
     @Override
-    public ResponseEntity<List<TerritoireBaseChefLieu>>  getcogdepsuiv(String code, LocalDate date) {
+    public ResponseEntity<List<TerritoireTousAttributs>>  getcogdepsuiv(String code, LocalDate date) {
         return requestProcessor.queryforFindPrecedentsSuivants()
                 .with(new PrecedentsSuivantsRequestParametizer(code, date, Departement.class, false))
                 .executeQuery()
-                .listResult(TerritoireBaseChefLieu.class)
+                .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
     }
 
@@ -86,14 +86,14 @@ public class GeoDepartementEndpoints implements GeoDepartementApi {
     }
 
     @Override
-    public ResponseEntity<List<TerritoireBaseChefLieu>> getcogdepts(String date) {
+    public ResponseEntity<List<TerritoireTousAttributs>> getcogdepts(String date) {
         if (date==null) {
             date = LocalDate.now().toString();
         }
         return requestProcessor.queryforFindTerritoire()
                 .with(new TerritoireEtoileRequestParametizer(date, Departement.class, "prefecture", true))
                 .executeQuery()
-                .listResult(TerritoireBaseChefLieu.class)
+                .listResult(TerritoireTousAttributs.class)
                 .toResponseEntity();
 
     }
