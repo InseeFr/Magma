6afbbce63bf14c1905fb00c7657c86b8bd5a6978
Tandeau De Marsac Guylène <C3JGPV@ad.insee.intra>
2025-11-04 16:34:17 +0100

    feat: for geo/quartiersPrioritairesDeLaVille2024

--- magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/requestprocessor/GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleEndpoints.java
+++ magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/requestprocessor/GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleEndpoints.java
@@ -2,11 +2,14 @@ package fr.insee.rmes.magma.diffusion.api.requestprocessor;
 
 import fr.insee.rmes.magma.diffusion.api.GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleApi;
 import fr.insee.rmes.magma.diffusion.model.QuartierPrioritaireDeLaPolitiqueDeLaVille2024;
+import fr.insee.rmes.magma.diffusion.model.TerritoireTousAttributs;
+import fr.insee.rmes.magma.diffusion.queries.parameters.TerritoireEtoileRequestParametizer;
 import fr.insee.rmes.magma.diffusion.queries.parameters.TerritoireRequestParametizer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleEndpoints implements GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleApi {
@@ -24,4 +27,18 @@ public class GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleEndpoints implements Ge
                 .executeQuery()
                 .singleResult(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.class).toResponseEntity();
     }
+
+    @Override
+    public ResponseEntity<List<QuartierPrioritaireDeLaPolitiqueDeLaVille2024>> getcogqpvliste (String date) {
+        if (date==null) {
+            date = LocalDate.now().toString();
+        }
+        return requestProcessor.queryforFindTerritoire()
+                .with(new TerritoireEtoileRequestParametizer(date, QuartierPrioritaireDeLaPolitiqueDeLaVille2024.class, "none"))
+                .executeQuery()
+                .listResult(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.class)
+                .toResponseEntity();
+    }
+
+
 }
--- magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/GeoQuartiersPrioritairesDeLaPolitiqueDeLaVilleTest.java
+++ magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/GeoQuartiersPrioritairesDeLaPolitiqueDeLaVilleTest.java
@@ -1,6 +1,8 @@
 package fr.insee.rmes.magma.diffusion.api.testcontainers.queries;
 
 import fr.insee.rmes.magma.diffusion.api.requestprocessor.GeoQuartierPrioritaireDeLaPolitiqueDeLaVilleEndpoints;
+import fr.insee.rmes.magma.diffusion.model.ArrondissementMunicipal;
+import fr.insee.rmes.magma.diffusion.model.CommuneAssociee;
 import fr.insee.rmes.magma.diffusion.model.QuartierPrioritaireDeLaPolitiqueDeLaVille2024;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
@@ -28,10 +30,10 @@ class GeoQuartiersPrioritairesDeLaPolitiqueDeLaVilleTest extends TestcontainerTe
     private MockMvc mockMvc;
 
     /// ///////////////////////////////////////////////////////////////////////
-    ///        geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024/{code}    ///
+    ///        geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024/{code}     ///
     /// ///////////////////////////////////////////////////////////////////////
 
-//    geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024/QN06255M?date=2025-09-04
+//    geo/quartiersPrioritairesDeLaPolitiqueDeLaVille2024/QN06255M?date=2025-09-04
     @Test
     void should_return_quartierPrioritaireDeLaVilleCode_QN06255M_when_codeQN06255M_date20250904() {
         var response = endpoints.getcogqpv("QN06255M", LocalDate.of(2025, 9, 4));
@@ -47,5 +49,49 @@ class GeoQuartiersPrioritairesDeLaPolitiqueDeLaVilleTest extends TestcontainerTe
                 () -> assertEquals("Ville Centre", result.getIntitule())
         );
     }
+
+    /// ///////////////////////////////////////////////////////////////////////
+    ///        geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024            ///
+    /// ///////////////////////////////////////////////////////////////////////
+
+//    geo/quartiersPrioritairesDeLaPolitiqueDeLaVille2024?date=2025-09-04//
+    @Test
+    void should_return_1609_QPV_when_quartierPrioritaireDeLaPolitiqueDeLaVille2024_date20250904() {
+        var response  = endpoints.getcogqpvliste("2025-09-04");
+        var result = response.getBody();
+        Assertions.assertNotNull(result);
+        var resultItem1= result.getFirst();
+
+        assertAll(
+                () -> assertEquals(1609, result.size()),
+                () -> assertEquals("QN00101M", resultItem1.getCode()),
+                () -> assertEquals("http://id.insee.fr/geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024/40526c49-2c78-4856-a4ed-21714bf70cb9", resultItem1.getUri()),
+                () -> assertEquals(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.TypeEnum.QUARTIER_PRIORITAIRE_DE_LA_POLITIQUE_DE_LA_VILLE2024, resultItem1.getType()),
+                () -> assertEquals(LocalDate.of(2024,1,1), resultItem1.getDateCreation()),
+                () -> assertEquals("Grande Reyssouze Terre Des Fleurs", resultItem1.getIntituleSansArticle()),
+                () -> assertEquals(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.TypeArticleEnum.X, resultItem1.getTypeArticle()),
+                () -> assertEquals("Grande Reyssouze Terre Des Fleurs", resultItem1.getIntitule())
+        );
+    }
+
+//    geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024?date=*
+    @Test
+    void should_return_1609_QPV_when_quartiersPrioritairesDeLaPolitiqueDeLaVille2024_dateEtoile() {
+        var response = endpoints.getcogqpvliste("*");
+        var result = response.getBody();
+        Assertions.assertNotNull(result);
+        var resultItem1 = result.getFirst();
+        assertAll(
+                () -> assertEquals(1609, result.size()),
+                () -> assertEquals("QN00101M", resultItem1.getCode()),
+                () -> assertEquals("http://id.insee.fr/geo/quartierPrioritaireDeLaPolitiqueDeLaVille2024/40526c49-2c78-4856-a4ed-21714bf70cb9", resultItem1.getUri()),
+                () -> assertEquals(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.TypeEnum.QUARTIER_PRIORITAIRE_DE_LA_POLITIQUE_DE_LA_VILLE2024, resultItem1.getType()),
+                () -> assertEquals(LocalDate.of(2024, 1, 1), resultItem1.getDateCreation()),
+                () -> assertEquals("Grande Reyssouze Terre Des Fleurs", resultItem1.getIntituleSansArticle()),
+                () -> assertEquals(QuartierPrioritaireDeLaPolitiqueDeLaVille2024.TypeArticleEnum.X, resultItem1.getTypeArticle()),
+                () -> assertEquals("Grande Reyssouze Terre Des Fleurs", resultItem1.getIntitule())
+        );
+    }
+
 
 }
