6e803067a9f03794477f11a3c94386c8ffc0a310
Tandeau De Marsac Guylène <C3JGPV@ad.insee.intra>
2026-01-15 16:14:09 +0100

    tests: add end to end tests

--- magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/OperationsQueriesTest.java
+++ magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/OperationsQueriesTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
@@ -14,6 +15,8 @@ import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,6 +32,9 @@ public class OperationsQueriesTest extends TestcontainerTest {
     /// //////////////////////////////////////////////////////////////////
     ///             operations/rapportQualite/{id}                     ///
     /// //////////////////////////////////////////////////////////////////
+
+    // "logique métier" test of a rapportQualite with all types of rubrics except ORGANIZATION
+    //    operations/rapportQualite/1979
 
     @Test
     void should_return_rapportQualite1979_when_OperationsRapportQualite_id1979() throws IOException {
@@ -153,8 +159,128 @@ public class OperationsQueriesTest extends TestcontainerTest {
         );
     }
 
+
+    //  end to end test of a rapportQualite with all types of rubrics except ORGANIZATION rubric
+    //    operations/rapportQualite/1979
     @Test
-    void should_return_rapportQualite1981_when_OperationsRapportQualite_id1981() throws IOException {
+    void should_return_rapportQualite1979_when_OperationsRapportQualite_id1979_endToEndTest() throws Exception {
+        mockMvc.perform(get("/operations/rapportQualite/1979"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                // Root level
+                .andExpect(jsonPath("$.id").value("1979"))
+                .andExpect(jsonPath("$.uri").value("http://id.insee.fr/qualite/rapport/1979"))
+                .andExpect(jsonPath("$.label[0].contenu").value("Rapport qualité : Enquête emploi en continu 2020"))
+                .andExpect(jsonPath("$.label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.label[1].contenu").value("Quality report: Labour force survey 2020"))
+                .andExpect(jsonPath("$.label[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques.length()").value(27))
+
+                // 1st rubric (TEXT type)
+                .andExpect(jsonPath("$.rubriques[0].id").value("I.6.4"))
+                .andExpect(jsonPath("$.rubriques[0].uri").value("http://id.insee.fr/qualite/simsv2fr/attribut/I.6.4"))
+                .andExpect(jsonPath("$.rubriques[0].idParent").value("S.6"))
+                .andExpect(jsonPath("$.rubriques[0].type").value("TEXT"))
+                .andExpect(jsonPath("$.rubriques[0].label[0].contenu").value("<p>2018T006EC</p>"))
+                .andExpect(jsonPath("$.rubriques[0].label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[0].label[1].contenu").value("<p>2018T006EC</p>"))
+                .andExpect(jsonPath("$.rubriques[0].label[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[0].titre[0].contenu").value("n° de visa"))
+                .andExpect(jsonPath("$.rubriques[0].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[0].titre[1].contenu").value("n° visa"))
+                .andExpect(jsonPath("$.rubriques[0].titre[1].langue").value("en"))
+
+                // 7th rubric (DATE type)
+                .andExpect(jsonPath("$.rubriques[6].id").value("S.2.3"))
+                .andExpect(jsonPath("$.rubriques[6].uri").value("http://ec.europa.eu/eurostat/simsv2/attribute/S.2.3"))
+                .andExpect(jsonPath("$.rubriques[6].idParent").value("S.2"))
+                .andExpect(jsonPath("$.rubriques[6].type").value("DATE"))
+                .andExpect(jsonPath("$.rubriques[6].titre[0].contenu").value("Dernière mise à jour des métadonnées"))
+                .andExpect(jsonPath("$.rubriques[6].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[6].titre[1].contenu").value("Metadata last update"))
+                .andExpect(jsonPath("$.rubriques[6].titre[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[6].date").value("2018-12-17"))
+
+                // 8th rubric (RICH_TEXT type)
+                .andExpect(jsonPath("$.rubriques[7].id").value("S.10.6"))
+                .andExpect(jsonPath("$.rubriques[7].uri").value("http://id.insee.fr/qualite/simsv2fr/attribut/S.10.6"))
+                .andExpect(jsonPath("$.rubriques[7].idParent").value("S.10"))
+                .andExpect(jsonPath("$.rubriques[7].type").value("RICH_TEXT"))
+                .andExpect(jsonPath("$.rubriques[7].titre[0].contenu").value("Documentation sur la méthodologie"))
+                .andExpect(jsonPath("$.rubriques[7].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].titre[1].contenu").value("Documentation on methodology"))
+                .andExpect(jsonPath("$.rubriques[7].titre[1].langue").value("en"))
+
+                // 8th rubric - first content
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].label[0].contenu").value("Note méthodologique EEC juillet 2021"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].label[1].contenu").value("LFS methodological note July 2021"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].label[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].dateMiseAJour").value("2021-07-13T00:00:00"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].documents[0].url").value("https://www.insee.fr/fr/metadonnees/source/fichier/EEC2021_Note-methodologique-juillet-2021.pdf"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].texte").value(""))
+                .andExpect(jsonPath("$.rubriques[7].contenus[0].langue").value("fr"))
+
+                // 8th rubric - last content
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].label[0].contenu").value("Note méthodologique EEC juillet 2021"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].label[1].contenu").value("LFS methodological note July 2021"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].label[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].dateMiseAJour").value("2021-07-13T00:00:00"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].documents[0].url").value("https://www.insee.fr/fr/metadonnees/source/fichier/EEC2021_Note-methodologique-juillet-2021.pdf"))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].texte").value(""))
+                .andExpect(jsonPath("$.rubriques[7].contenus[1].langue").value("en"))
+
+                // 10th rubric (GEOGRAPHY type)
+                .andExpect(jsonPath("$.rubriques[9].id").value("S.3.7"))
+                .andExpect(jsonPath("$.rubriques[9].uri").value("http://id.insee.fr/qualite/simsv2fr/attribut/S.3.7"))
+                .andExpect(jsonPath("$.rubriques[9].idParent").value("S.3"))
+                .andExpect(jsonPath("$.rubriques[9].type").value("GEOGRAPHY"))
+                .andExpect(jsonPath("$.rubriques[9].titre[0].contenu").value("Zone géographique de référence"))
+                .andExpect(jsonPath("$.rubriques[9].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[9].titre[1].contenu").value("Reference area"))
+                .andExpect(jsonPath("$.rubriques[9].titre[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[9].territoire.id").value("franceHorsMayotte"))
+                .andExpect(jsonPath("$.rubriques[9].territoire.uri").value("http://id.insee.fr/qualite/territoire/franceHorsMayotte"))
+                .andExpect(jsonPath("$.rubriques[9].territoire.label[0].contenu").value("France hors Mayotte"))
+                .andExpect(jsonPath("$.rubriques[9].territoire.label[0].langue").value("fr"))
+
+                // 13th rubric (CODE_LIST type)
+                .andExpect(jsonPath("$.rubriques[12].id").value("I.18.8"))
+                .andExpect(jsonPath("$.rubriques[12].uri").value("http://id.insee.fr/qualite/simsv2fr/attribut/I.18.8"))
+                .andExpect(jsonPath("$.rubriques[12].idParent").value("S.18"))
+                .andExpect(jsonPath("$.rubriques[12].type").value("CODE_LIST"))
+                .andExpect(jsonPath("$.rubriques[12].titre[0].contenu").value("Mode de collecte"))
+                .andExpect(jsonPath("$.rubriques[12].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[12].titre[1].contenu").value("Collection mode"))
+                .andExpect(jsonPath("$.rubriques[12].titre[1].langue").value("en"))
+
+                // 13th rubric - first code
+                .andExpect(jsonPath("$.rubriques[12].codes[0].id").value("F"))
+                .andExpect(jsonPath("$.rubriques[12].codes[0].uri").value("http://id.insee.fr/codes/modeCollecte/F"))
+                .andExpect(jsonPath("$.rubriques[12].codes[0].label[0].contenu").value("Face à face par enquêteur"))
+                .andExpect(jsonPath("$.rubriques[12].codes[0].label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[12].codes[0].label[1].contenu").value("Face to face by interviewer"))
+                .andExpect(jsonPath("$.rubriques[12].codes[0].label[1].langue").value("en"))
+
+                // 13th rubric - last code
+                .andExpect(jsonPath("$.rubriques[12].codes[1].id").value("P"))
+                .andExpect(jsonPath("$.rubriques[12].codes[1].uri").value("http://id.insee.fr/codes/modeCollecte/P"))
+                .andExpect(jsonPath("$.rubriques[12].codes[1].label[0].contenu").value("Par téléphone"))
+                .andExpect(jsonPath("$.rubriques[12].codes[1].label[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[12].codes[1].label[1].contenu").value("By phone"))
+                .andExpect(jsonPath("$.rubriques[12].codes[1].label[1].langue").value("en"));
+    }
+
+
+    // "logique métier" test of an ORGANIZATION rubric
+    //    operations/rapportQualite/1981
+
+    @Test
+    void should_return_rapportQualite1981_when_OperationsRapportQualite_id1981_BusinessLogicTest() throws IOException {
         var response = endpoints.getRapportQualiteByCode("1981");
         var result = response.getBody();
         Assertions.assertNotNull(result);
@@ -179,5 +305,30 @@ public class OperationsQueriesTest extends TestcontainerTest {
                 }
         );
     }
+
+
+    //  end to end test of an ORGANIZATION rubric
+    //    operations/rapportQualite/1981
+    @Test
+    void should_return_organizationRubric_when_OperationsRapportQualite_id1981_endToEndTest() throws Exception {
+        mockMvc.perform(get("/operations/rapportQualite/1981"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.rubriques[7].id").value("S.1.2"))
+                .andExpect(jsonPath("$.rubriques[7].uri").value("http://id.insee.fr/qualite/simsv2fr/attribut/S.1.2"))
+                .andExpect(jsonPath("$.rubriques[7].idParent").value("S.1"))
+                .andExpect(jsonPath("$.rubriques[7].type").value("ORGANIZATION"))
+                .andExpect(jsonPath("$.rubriques[7].titre[0].contenu").value("Unité d'appartenance du  contact dans l'organisme"))
+                .andExpect(jsonPath("$.rubriques[7].titre[0].langue").value("fr"))
+                .andExpect(jsonPath("$.rubriques[7].titre[1].contenu").value("Contact organisation unit"))
+                .andExpect(jsonPath("$.rubriques[7].titre[1].langue").value("en"))
+                .andExpect(jsonPath("$.rubriques[7].organisme.id").value("DG75-E330"))
+                .andExpect(jsonPath("$.rubriques[7].organisme.uri").value("http://id.insee.fr/organisations/insee/HIE2000265"))
+                .andExpect(jsonPath("$.rubriques[7].organisme.label[0].contenu").value("Division Elaboration des statistiques de production industrielle (ESPRI)"))
+                .andExpect(jsonPath("$.rubriques[7].organisme.label[0].langue").value("fr"));
+
+    }
+
+
 }
 
