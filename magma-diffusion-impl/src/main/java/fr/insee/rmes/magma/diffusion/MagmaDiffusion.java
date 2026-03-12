3b02ae1af31bea399b8998a5d1ac28d3514ab09a
GtanSndil <C3JGPV@ad.insee.intra>
2026-03-12 15:29:22 +0100

    feat: change name of main class for MagmaDiffusion

--- magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/MagmaDiffusion.java
+++ magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/MagmaDiffusion.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Magma {
+public class MagmaDiffusion {
     public static void main(String[] args) {
 
-        SpringApplication.run(Magma.class, args);
+        SpringApplication.run(MagmaDiffusion.class, args);
     }
 }
