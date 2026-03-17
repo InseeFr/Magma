473b3b2af94dc7d4f03653c2913d5c45f8636695
GtanSndil <C3JGPV@ad.insee.intra>
2026-03-17 14:23:21 +0100

    cconfig: to launch MagmaAppMain and tests
    classes in comment to delete later

---
+++ magma-commons-infra/src/main/java/fr/insee/rmes/magma/config/MagmaConfig.java
@@ -0,0 +1,57 @@
+package fr.insee.rmes.magma.config;
+
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.MultiTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.TemplateModelException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Locale;
+
+@org.springframework.context.annotation.Configuration
+@Slf4j
+@ServletComponentScan
+public class MagmaConfig {
+
+    @Bean
+    public Configuration freemarkerConfiguration
+            (@Value("${fr.insee.rmes.magma.api.freemarker.locale-language:fr}") String localLanguage,
+            @Value("${fr.insee.rmes.magma.baseGraph:http://rdf.insee.fr/graphes/}") String baseGraph,
+            @Value("${fr.insee.rmes.magma.codeLists.graph:codes}") String codeListsGraph,
+            @Value("${fr.insee.rmes.magma.structures.graph:structures}") String structuresGraph,
+            @Value("${fr.insee.rmes.magma.lg1:fr}") String lg1,
+            @Value("${fr.insee.rmes.magma.lg2:en}") String lg2) throws URISyntaxException, IOException, TemplateModelException {
+        var configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+        // Charge les templates depuis le classpath (dans src/main/resources/request)
+        ClassTemplateLoader classTemplateLoader = new ClassTemplateLoader(MagmaConfig.class, "/request");
+
+        MultiTemplateLoader mtl = new MultiTemplateLoader(new TemplateLoader[]{classTemplateLoader});
+
+        //log.info("Init freemarker templateloader {}", freemarkerTemplatesDirectory);
+        configuration.setTemplateLoader(mtl);
+
+        configuration.setDefaultEncoding("UTF-8");
+        configuration.setLocale(Locale.of(localLanguage));
+
+        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        configuration.setLogTemplateExceptions(false);
+
+        configuration.setWrapUncheckedExceptions(true);
+
+        configuration.setSharedVariable("CODELIST_GRAPH", baseGraph + codeListsGraph);
+        configuration.setSharedVariable("STRUCTURES_GRAPH", baseGraph + structuresGraph);
+        configuration.setSharedVariable("LG1", lg1);
+        configuration.setSharedVariable("LG2", lg2);
+
+        return configuration;
+    }
+}
+
