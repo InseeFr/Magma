2106c9f7e5f0c7c0cecc2cd61b3240d3024ba951
GtanSndil <C3JGPV@ad.insee.intra>
2026-03-23 12:03:35 +0100

    fix: intersect

--- magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/GeoCantonEndpoints.java
+++ magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/GeoCantonEndpoints.java
@@ -1,11 +1,9 @@
 package fr.insee.rmes.magma.diffusion.api;
 
-
 import fr.insee.rmes.magma.diffusion.api.requestprocessor.RequestProcessor;
 import fr.insee.rmes.magma.diffusion.model.*;
 import fr.insee.rmes.magma.diffusion.queries.parameters.*;
 import fr.insee.rmes.magma.diffusion.utils.EndpointsUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -99,8 +97,9 @@ public class GeoCantonEndpoints implements GeoCantonApi {
 
     @Override
     public ResponseEntity<List<TerritoireBaseRelation>> getcogcanintersect (String code, LocalDate date, TypeEnum type) {
+        String territoriesFilter = this.endpointsUtils.defineTerritoriesFilter(type == null ? null : type.getValue());
         return requestProcessor.queryToFindIntersections()
-                .with(new TerritoiresLiesRequestParametizer(code, date, type, Canton.class))
+                .with(new TerritoiresLiesRequestParametizer(code, date, territoriesFilter, Canton.class))
                 .executeQuery()
                 .listResult(TerritoireBaseRelation.class)
                 .toResponseEntity();
