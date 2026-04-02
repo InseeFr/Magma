59af95ac59d971d41265b0fb51dbd1b94f0d45fd
GtanSndil <C3JGPV@ad.insee.intra>
2026-04-02 09:13:38 +0200

    Revert "feat: delete run MagmaDiffusion and MagmaGestion (keep MagmaAppAMin)"
    This reverts commit 5e0af84268b599a49296021749745b04d67f8976.

---
+++ magma-gestion-impl/src/main/java/fr/insee/rmes/magma/MagmaGestion.java
@@ -0,0 +1,12 @@
+package fr.insee.rmes.magma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagmaGestion {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MagmaGestion.class, args);
+    }
+}
