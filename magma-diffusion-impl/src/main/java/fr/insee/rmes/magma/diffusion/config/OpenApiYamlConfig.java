a6d8f5c94d1ed0f018d7a9c6c463b38d54ad0e27
GtanSndil <mail@ad.insee.intra>
2026-06-08 12:16:20 +0200

    style: try to have examples from openapi files (gestion and diffusion)

---
+++ magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/config/OpenApiYamlConfig.java
@@ -0,0 +1,27 @@
+package fr.insee.rmes.magma.diffusion.config;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenApiYamlConfig {
+
+    @GetMapping(value = "/openapi-diffusion.yaml", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<Resource> openApiYaml() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(new ClassPathResource("openapi-diffusion.yaml"));
+    }
+
+    @GetMapping(value = "/openapi-gestion.yaml", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<Resource> openApiGestionYaml() {
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(new ClassPathResource("openapi-gestion.yaml"));
+    }
+
+}
