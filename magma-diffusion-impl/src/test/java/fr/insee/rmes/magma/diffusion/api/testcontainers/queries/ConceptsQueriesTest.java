35c4d5cce588a8cb5920b4a01bae1df56d9d7dc2
GtanSndil <mail@ad.insee.intra>
2026-07-01 12:04:56 +0200

    tests

--- magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/ConceptsQueriesTest.java
+++ magma-diffusion-impl/src/test/java/fr/insee/rmes/magma/diffusion/api/testcontainers/queries/ConceptsQueriesTest.java
@@ -244,7 +244,7 @@ public class ConceptsQueriesTest extends TestcontainerTest {
         var resultItem1= result.getFirst();
 
         assertAll(
-                () -> assertEquals(1233, result.size()),
+                () -> assertEquals(1237, result.size()),
                 () -> assertEquals("c1601", resultItem1.getId()),
                 () -> assertEquals(URI.create("http://id.insee.fr/concepts/definition/c1601"), resultItem1.getUri()),
                 () -> assertEquals("ADSL", resultItem1.getIntitule())
