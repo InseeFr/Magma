f0043baf8ea7f4af3a1d1ad1cd66a6091b7e3dea
GtanSndil <mail@ad.insee.intra>
2026-07-02 15:52:55 +0200

    style: remove unused import

--- magma-gestion-impl/src/main/java/fr/insee/rmes/magma/gestion/services/DatasetsService.java
+++ magma-gestion-impl/src/main/java/fr/insee/rmes/magma/gestion/services/DatasetsService.java
@@ -3,7 +3,6 @@ package fr.insee.rmes.magma.gestion.services;
 import fr.insee.rmes.magma.gestion.model.Dataset;
 import fr.insee.rmes.magma.gestion.model.Distribution;
 import fr.insee.rmes.magma.gestion.utils.DatasetByIdDTO;
-import fr.insee.rmes.magma.gestion.utils.DatasetByIdSummaryDTO;
 import fr.insee.rmes.magma.gestion.utils.DatasetDTO;
 import fr.insee.rmes.magma.gestion.utils.DistributionDTO;
 
