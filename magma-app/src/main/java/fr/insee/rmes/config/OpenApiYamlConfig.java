0dedfa60cb23c87926498661f4f14bcfd241ddb7
GtanSndil <mail@ad.insee.intra>
2026-07-02 17:39:54 +0200

    style: afficher seulement les end-points de gestion ou seulement de diffusion
    en fonction de la valeur d'une property, afficher seulement les end-points de openapi-diffusion.yaml ou seulement ceux de openapi-gestion.yaml (plus de dropdown en haut à droite  dans le Swagger UI permettant de switcher entre les deux specs.)

--- magma-app/src/main/java/fr/insee/rmes/config/OpenApiYamlConfig.java
+++ magma-app/src/main/java/fr/insee/rmes/config/OpenApiYamlConfig.java
@@ -1,28 +1,25 @@
 package fr.insee.rmes.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenApiYamlConfig {
 
-    @GetMapping(value = "/openapi-diffusion.yaml", produces = "text/plain;charset=UTF-8")
-    public ResponseEntity<Resource> openApiYaml() {
-        return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_PLAIN)
-                .body(new ClassPathResource("openapi-diffusion.yaml"));
-    }
+    @Value("${fr.insee.rmes.magma.api.sparqlEndpoint.module}")
+    private String module;
 
-    @GetMapping(value = "/openapi-gestion.yaml", produces = "text/plain;charset=UTF-8")
-    public ResponseEntity<Resource> openApiGestionYaml() {
+    @GetMapping(value = "/openapi.yaml", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<Resource> openApiYaml() {
+        String fileName = "gestion".equals(module) ? "openapi-gestion.yaml" : "openapi-diffusion.yaml";
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_PLAIN)
-                .body(new ClassPathResource("openapi-gestion.yaml"));
+                .body(new ClassPathResource(fileName));
     }
 
 
