685259acc9154c5a4a9fbeee92eddcc1387b4a23
GtanSndil <mail@ad.insee.intra>
2026-09-03 16:08:49 +0200

    fix: roll back for operationsEndpoints

--- magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/OperationsEndpoints.java
+++ magma-diffusion-impl/src/main/java/fr/insee/rmes/magma/diffusion/api/OperationsEndpoints.java
@@ -8,11 +8,11 @@ import fr.insee.rmes.magma.diffusion.services.RapportQualiteService;
 import fr.insee.rmes.magma.diffusion.utils.RapportQualiteDTO;
 import fr.insee.rmes.magma.diffusion.utils.RubriqueDTO;
 import fr.insee.rmes.magma.utils.EndpointsUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 public class OperationsEndpoints implements OperationsApi {
 
@@ -24,19 +24,7 @@ public class OperationsEndpoints implements OperationsApi {
         this.rapportQualiteService = rapportQualiteService;
     }
 
-    @Value("${fr.insee.rmes.magma.uri}")
-    private String magmaUri;
-    @Value("${fr.insee.rmes.magma.lg1}")
-    private String magmaLg1;
-    @Value("${fr.insee.rmes.magma.lg2}")
-    private String magmaLg2;
 
-    private String getLg1Cl() {
-        return "http://" + magmaUri + "/codes/langue/" + magmaLg1 ;
-    }
-    private String getLg2Cl() {
-        return "http://" + magmaUri + "/codes/langue/" + magmaLg2;
-    }
     @Override
     public ResponseEntity<RapportQualite> getRapportQualiteByCode(String idSims) {
         RapportQualiteDTO rapportQualiteDTO = requestProcessorDiffusion.queryToFindRapportQualite()
@@ -49,8 +37,11 @@ public class OperationsEndpoints implements OperationsApi {
             return ResponseEntity.notFound().build();
         }
 
+        String LG1_CL = "http://id.insee.fr/codes/langue/fr";
+        String LG2_CL = "http://id.insee.fr/codes/langue/en";
+
         List<RubriqueDTO> rubriqueList = requestProcessorDiffusion.queryToFindRubriques()
-                .with(new OperationRubriquesRequestParametizer(rapportQualiteDTO.id(), getLg1Cl(), getLg2Cl()))
+                .with(new OperationRubriquesRequestParametizer(rapportQualiteDTO.id(), LG1_CL, LG2_CL))
                 .executeQuery()
                 .listResult(RubriqueDTO.class)
                 .result();
